a9b943bed21e4460634fad11663c3aeafe8fc8aa
Parth Tiwari <parthtiwari321@gmail.com>
2025-12-10 12:50:44 +0530

    dto

--- src/main/java/com/assesment/inventory_management/dto/ErrorResponse.java
+++ src/main/java/com/assesment/inventory_management/dto/ErrorResponse.java
@@ -4,17 +4,9 @@ import java.time.LocalDateTime;
 
 public class ErrorResponse {
     private LocalDateTime timestamp;
-    private String message;
-    private String path;
 
     public ErrorResponse() {
         this.timestamp = LocalDateTime.now();
-    }
-
-    public ErrorResponse(String message, String path) {
-        this.timestamp = LocalDateTime.now();
-        this.message = message;
-        this.path = path;
     }
 
     public LocalDateTime getTimestamp() {
@@ -23,21 +15,5 @@ public class ErrorResponse {
 
     public void setTimestamp(LocalDateTime timestamp) {
         this.timestamp = timestamp;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
     }
 }
